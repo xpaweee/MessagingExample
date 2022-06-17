0f11930c276afe5419c52ee0d2e66f31c4dc4eb0
xpaweee <krzysiek.kolsut@gmail.com>
2022-06-17 17:21:19 +0200

    init

---
+++ src/SuperStore.Carts/Program.cs
@@ -0,0 +1,17 @@
+//using SuperStore.Shared;
+//using SuperStore.Shared.Deduplication;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddMessaging(c => c.AddDeduplication<CartsDbContext>(builder.Configuration));
+//builder.Services.AddDataAccess();
+//builder.Services.AddHostedService<MessagingBackgroundService>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Carts Service!");
+
+var scope = app.Services.CreateScope();
+//var ctx = scope.ServiceProvider.GetRequiredService<CartsDbContext>();
+//ctx.Database.EnsureCreated();
+
+app.Run();
---
+++ src/SuperStore.Funds/Program.cs
@@ -0,0 +1,14 @@
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddMessaging();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Funds Service!");
+//app.MapGet("/message/send/EU/{country}", async (IMessagePublisher messagePublisher, string country, string messageId) =>
+//{
+//    var message = new FundsMessage(123, 10.00m);
+//    await messagePublisher.PublishAsync("Funds", $"EU.{country}", message, messageId);
+//});
+
+app.Run();
